9ca7ec4b54165d199657453e8f77f452273a6615
moaadhzair <92125533+moaadhzair@users.noreply.github.com>
2025-05-13 00:29:03 +0100

    some changes don't know if it works tho

--- lib/pages/player_page.dart
+++ lib/pages/player_page.dart
@@ -1,11 +1,9 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-
 import 'package:video_player/video_player.dart';
+import 'package:flutter/services.dart'; // <-- Add this
 
 class PlayerPage extends StatefulWidget {
   const PlayerPage({super.key, required this.StreamData});
-
   final List<dynamic> StreamData;
 
   @override
@@ -13,32 +11,117 @@ class PlayerPage extends StatefulWidget {
 }
 
 class _PlayerPageState extends State<PlayerPage> {
-  late VideoPlayerController videoPlayerController;
+  late VideoPlayerController _controller;
+  bool _isPlaying = false;
+  bool _showControls = true;
 
   @override
   void initState() {
     super.initState();
-    initializeVideoPlayer();
+
+    // Force landscape mode
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+
+    _controller = VideoPlayerController.network(widget.StreamData[0]["m3u8"])
+      /*..initialize().then((_) {
+        setState(() {});
+      })*/;
+
+    _controller.addListener(() {
+      if (_controller.value.hasError) {
+        debugPrint("Video error: ${_controller.value.errorDescription}");
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+
+    // Reset to normal orientations when exiting
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    super.dispose();
+  }
+
+  void togglePlayPause() {
+    setState(() {
+      _isPlaying ? _controller.pause() : _controller.play();
+      _isPlaying = !_isPlaying;
+    });
+  }
+
+  Widget buildControls() {
+    return AnimatedOpacity(
+      opacity: _showControls ? 1.0 : 0.0,
+      duration: const Duration(milliseconds: 300),
+      child: Container(
+        color: Colors.black38,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            VideoProgressIndicator(
+              _controller,
+              allowScrubbing: true,
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              colors: const VideoProgressColors(
+                playedColor: Colors.red,
+                bufferedColor: Colors.white70,
+                backgroundColor: Colors.grey,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  icon: Icon(
+                    _isPlaying ? Icons.pause : Icons.play_arrow,
+                    color: Colors.white,
+                  ),
+                  onPressed: togglePlayPause,
+                ),
+                IconButton(
+                  icon: const Icon(Icons.fullscreen_exit, color: Colors.white),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Player')),
+      backgroundColor: Colors.black,
       body: Center(
-        child: AspectRatio(
-          aspectRatio: videoPlayerController.value.aspectRatio,
-          child: VideoPlayer(videoPlayerController),
-        ),
+        child:
+            _controller.value.isInitialized
+                ? GestureDetector(
+                  onTap: () => setState(() => _showControls = !_showControls),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      AspectRatio(
+                        aspectRatio: _controller.value.aspectRatio,
+                        child: VideoPlayer(_controller),
+                      ),
+                      if (_showControls) buildControls(),
+                    ],
+                  ),
+                )
+                : const CircularProgressIndicator(),
       ),
     );
-  }
-  
-  void initializeVideoPlayer() async{
-    videoPlayerController = VideoPlayerController.network(
-      widget.StreamData[0]["m3u8"],
-    );
-    await videoPlayerController.initialize();
-    setState(() {});
   }
 }
